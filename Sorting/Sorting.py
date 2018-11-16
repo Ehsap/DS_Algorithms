76ceb9c058f4bd9ac7223ff932114a40aebd9240
Justin Huynh <polar.truth@gmail.com>
2018-11-16 11:09:35 -0500

    Initial Commit

---
+++ Sorting/Sorting.py
@@ -0,0 +1,20 @@
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+	return alist
+def quicksort(a):
+	pass
+
+def mergesort(a):
+	pass 
+	
+def heapsort(a):
+	pass 
